28602b4e4e03d5ba808fb9d6d0f8a341c01fc25d
bgn9000 <pbourgeon@gmail.com>
2013-02-24 22:00:40 +0100

    Added Abyssplug

--- build_kernel.sh
+++ build_kernel.sh
@@ -32,7 +32,7 @@ nice -n 10 make -j4 || exit 1
 #remove previous ramfs files
 rm -rf $RAMFS_TMP
 rm -rf $RAMFS_TMP.cpio
-rm -rf $RAMFS_TMP.cpio.gz
+rm -rf $RAMFS_TMP.cpio.xz
 #copy ramfs files to tmp directory
 cp -ax $RAMFS_SOURCE $RAMFS_TMP
 #clear git repositories in ramfs
@@ -52,14 +52,14 @@ ${CROSS_COMPILE}strip --strip-unneeded $RAMFS_TMP/lib/modules/*
 cd $RAMFS_TMP
 find | fakeroot cpio -H newc -o > $RAMFS_TMP.cpio 2>/dev/null
 ls -lh $RAMFS_TMP.cpio
-gzip -9 $RAMFS_TMP.cpio
-#xz --check=crc32 --lzma2=dict=1MiB $RAMFS_TMP.cpio
+#gzip -9 $RAMFS_TMP.cpio
+xz --check=crc32 --lzma2=dict=1MiB $RAMFS_TMP.cpio
 cd -
 
 nice -n 10 make -j3 zImage || exit 1
 
-./mkbootimg --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk $RAMFS_TMP.cpio.gz --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNELDIR/boot.img.pre
+./mkbootimg --kernel $KERNELDIR/arch/arm/boot/zImage --ramdisk $RAMFS_TMP.cpio.xz --board smdk4x12 --base 0x10000000 --pagesize 2048 --ramdiskaddr 0x11000000 -o $KERNELDIR/boot.img.pre
 
 $KERNELDIR/mkshbootimg.py $KERNELDIR/boot.img $KERNELDIR/boot.img.pre $KERNELDIR/payload.tar
-#rm -f $KERNELDIR/boot.img.pre
+rm -f $KERNELDIR/boot.img.pre
 
